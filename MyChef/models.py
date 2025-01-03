667375f682b3f93a459f8ff83f92d3b99a1ce86a
RoaringRoyel <minhazulroyel001@gmail.com>
2025-01-03 12:38:59 +0600

    db for recipe

--- MyChef/models.py
+++ MyChef/models.py
@@ -9,3 +9,31 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(150), nullable=False)
     password = db.Column(db.String(150), nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+
+########## DB FOR RECIPE ##########
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    cuisine = db.Column(db.String(100), nullable=True)
+    image = db.Column(db.String(150), nullable=True)
+    rating = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    
+    # Relationships
+    ingredients = db.relationship('Ingredient', backref='recipe', cascade='all, delete-orphan')
+    instructions = db.relationship('Instruction', backref='recipe', cascade='all, delete-orphan')
+
+
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    ingredient = db.Column(db.String(150), nullable=False)
+
+
+class Instruction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    step_number = db.Column(db.Integer, nullable=False)
+    instruction = db.Column(db.Text, nullable=False)
+##############################################
